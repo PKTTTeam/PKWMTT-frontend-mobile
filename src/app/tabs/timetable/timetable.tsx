3f187396538385e828188258c58ccc411bc9b143
Ereffe <eriffe9@gmail.com>
2025-07-07 19:59:23 +0200

    Delete unused imports from timetable (for now)

--- src/app/tabs/timetable/timetable.tsx
+++ src/app/tabs/timetable/timetable.tsx
@@ -1,16 +1,9 @@
-import {
-  Text,
-  View,
-  FlatList,
-  ListRenderItemInfo,
-  RefreshControl,
-  TouchableOpacity,
-} from 'react-native';
+import { View, Text } from 'react-native';
 const Timetable = () => {
   return (
-    <View
-      style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
-    ></View>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Timetable</Text>
+    </View>
   );
 };
 
