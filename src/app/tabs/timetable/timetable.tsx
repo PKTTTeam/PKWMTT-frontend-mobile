e654f24f5346a35b159418afae8006a4a5d35dda
SharpP03 <Patrykstoncel186@wp.pl>
2025-07-14 20:35:46 +0200

    Component LetterIcon added - in progress.
    Displays a letter in cricle
    params:
    color as color, letter as char, letterColor as color.

--- src/app/tabs/timetable/timetable.tsx
+++ src/app/tabs/timetable/timetable.tsx
@@ -1,11 +1,14 @@
 import { View, Text } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
+import LetterIcon from '../../../components/ui/letterIcon';
+
 const Timetable = () => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Timetable</Text>
       <Icon name="schedule" size={30} color={'blue'} />
+      <LetterIcon color='black' letter='X' letterColor='red'/>
     </View>
   );
 };
