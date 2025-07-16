42e0185a70eda1a3d638a21088f72fa58e9c2efd
Ereffe <eriffe9@gmail.com>
2025-07-17 00:40:33 +0200

    feat: add LetterIcon component to TimetableScreen

--- src/app/tabs/TimetableScreen/TimetableScreen.tsx
+++ src/app/tabs/TimetableScreen/TimetableScreen.tsx
@@ -3,6 +3,7 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 import TimeRange from '../../../components/ui/TimeRange';
 import RoomInfo from '../../../components/ui/RoomInfo';
 import globalStyles from '../../../styles/globalStyles';
+import LetterIcon from '../../../components/ui/letterIcon';
 
 const TimetableScreen = () => {
   return (
@@ -11,6 +12,7 @@ const TimetableScreen = () => {
       <Icon name="schedule" size={30} color={'blue'} />
       <TimeRange timeStart={'12:30'} timeEnd={'14:30'} />
       <RoomInfo room={'J207'} />
+      <LetterIcon />
     </View>
   );
 };
