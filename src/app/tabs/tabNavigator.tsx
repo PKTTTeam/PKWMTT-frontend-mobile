bf21c3a9caea84d0b752ee6442d0efa155ee1963
Ereffe <eriffe9@gmail.com>
2025-07-17 00:29:06 +0200

    fix: update header styles in TabNavigator

--- src/app/tabs/tabNavigator.tsx
+++ src/app/tabs/tabNavigator.tsx
@@ -17,6 +17,14 @@ const screenOptions = {
   tabBarInactiveBackgroundColor: '#161514',
   tabBarActiveBackgroundColor: '#161514',
 
+  headerStyle: {
+    backgroundColor: '#181818',
+  },
+
+  headerTitleStyle: {
+    color: '#FFFFFF',
+  },
+
   tabBarStyle: {
     height: 100,
   },
