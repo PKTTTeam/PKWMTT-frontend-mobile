b0b9b9348da0cb7eac2c64f64ad22deebb708ad4
SharpP03 <Patrykstoncel186@wp.pl>
2025-07-31 22:57:22 +0200

    Settings screen merge

--- src/app/tabs/SettingsScreen/SettingsScreen.tsx
+++ src/app/tabs/SettingsScreen/SettingsScreen.tsx
@@ -92,15 +92,4 @@ function SettingsScreen() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 16,
-  },
-});
-
 export default SettingsScreen;
