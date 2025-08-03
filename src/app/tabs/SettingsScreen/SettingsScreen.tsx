a1b1b2ccfefc57357beb550c6a9effa2cb5c421d
Ereffe <eriffe9@gmail.com>
2025-08-03 16:59:33 +0200

    Add ScrollView to SettingsScreen

--- src/app/tabs/SettingsScreen/SettingsScreen.tsx
+++ src/app/tabs/SettingsScreen/SettingsScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
 
 import GlobalStyles from '../../../styles/globalStyles.ts';
@@ -13,79 +13,90 @@ function SettingsScreen() {
     <>
       <PaperProvider>
         <View style={SettingsStyles.bgContainer}>
-          <View style={SettingsStyles.container}>
-            <Text style={SettingsStyles.labelText}>Grupy Studenckie</Text>
-            <View
-              style={[
-                SettingsStyles.studentGroups,
-                SettingsStyles.elementsSpacing,
-              ]}
-            >
-              <GroupSelect groupName="Dziekańska" />
-              <GroupSelect groupName="Laboratoryjna" />
-              <GroupSelect groupName="Komputerowa" />
-            </View>
-            <View
-              style={[
-                SettingsStyles.studentGroups,
-                SettingsStyles.elementsSpacing,
-              ]}
-            >
-              <GroupSelect groupName="Projektowa" />
-              <GroupSelect groupName="Ćwiczeniowa" />
-            </View>
-            <Text
-              style={[SettingsStyles.labelText, SettingsStyles.elementsSpacing]}
-            >
-              Powiadomienia
-            </Text>
-            <View>
-              <View style={SettingsStyles.notifications}>
-                <Switch label="Egzamin" />
-                <Switch label="Kolokwium" />
-              </View>
+          <ScrollView contentContainerStyle={SettingsStyles.scrollContent}>
+            <View style={SettingsStyles.container}>
+              <Text style={SettingsStyles.labelText}>Grupy Studenckie</Text>
               <View
                 style={[
-                  SettingsStyles.notifications,
+                  SettingsStyles.studentGroups,
                   SettingsStyles.elementsSpacing,
                 ]}
               >
-                <Switch label="Zaliczenie" />
-                <View style={SettingsStyles.elementsSpacing}>
-                  <Switch label="Projekt       " />
-                </View>
+                <GroupSelect groupName="Dziekańska" />
+                <GroupSelect groupName="Laboratoryjna" />
+                <GroupSelect groupName="Komputerowa" />
               </View>
               <View
                 style={[
-                  SettingsStyles.notificationsMid,
+                  SettingsStyles.studentGroups,
                   SettingsStyles.elementsSpacing,
                 ]}
               >
-                <Switch label="Aktualizacje rozkładu" />
-                <View style={SettingsStyles.groupsContainer}>
-                  <Text style={GlobalStyles.text}>Przypomnij przed</Text>
-                  <GroupSelect groupName="" />
+                <GroupSelect groupName="Projektowa" />
+                <GroupSelect groupName="Ćwiczeniowa" />
+              </View>
+              <Text
+                style={[
+                  SettingsStyles.labelText,
+                  SettingsStyles.elementsSpacing,
+                ]}
+              >
+                Powiadomienia
+              </Text>
+              <View>
+                <View style={SettingsStyles.notifications}>
+                  <Switch label="Egzamin" />
+                  <Switch label="Kolokwium" />
+                </View>
+                <View
+                  style={[
+                    SettingsStyles.notifications,
+                    SettingsStyles.elementsSpacing,
+                  ]}
+                >
+                  <Switch label="Zaliczenie" />
+                  <View style={SettingsStyles.elementsSpacing}>
+                    <Switch label="Projekt       " />
+                  </View>
+                </View>
+                <View
+                  style={[
+                    SettingsStyles.notificationsMid,
+                    SettingsStyles.elementsSpacing,
+                  ]}
+                >
+                  <Switch label="Aktualizacje rozkładu" />
+                  <View style={SettingsStyles.groupsContainer}>
+                    <Text style={GlobalStyles.text}>Przypomnij przed</Text>
+                    <GroupSelect groupName="" />
+                  </View>
                 </View>
               </View>
+              <Text
+                style={[
+                  SettingsStyles.labelText,
+                  SettingsStyles.elementsSpacing,
+                ]}
+              >
+                Wygląd Aplikacji
+              </Text>
+              <View style={SettingsStyles.elementsSpacing}>
+                <Switch label="Tryb ciemny" />
+              </View>
+              <View style={SettingsStyles.elementsSpacing}>
+                <Switch label="Czcionka powiększona" />
+              </View>
+              <Text
+                style={[
+                  SettingsStyles.labelText,
+                  SettingsStyles.elementsSpacing,
+                ]}
+              >
+                Język Aplikacji
+              </Text>
+              <GroupSelect groupName="" listPosUp={true} />
             </View>
-            <Text
-              style={[SettingsStyles.labelText, SettingsStyles.elementsSpacing]}
-            >
-              Wygląd Aplikacji
-            </Text>
-            <View style={SettingsStyles.elementsSpacing}>
-              <Switch label="Tryb ciemny" />
-            </View>
-            <View style={SettingsStyles.elementsSpacing}>
-              <Switch label="Czcionka powiększona" />
-            </View>
-            <Text
-              style={[SettingsStyles.labelText, SettingsStyles.elementsSpacing]}
-            >
-              Język Aplikacji
-            </Text>
-            <GroupSelect groupName="" listPosUp={true} />
-          </View>
+          </ScrollView>
         </View>
       </PaperProvider>
     </>
