869c58837615e3301f9280db491fb76b32b19453
Mikołaj Florczak <84631301+florczaq@users.noreply.github.com>
2025-11-07 23:04:07 +0100

    Refactor GroupCard component: reorganize imports, move styles to a separate file, and enhance code structure

--- src/app/tabs/SettingsScreen/components/GroupCard.tsx
+++ src/app/tabs/SettingsScreen/components/GroupCard.tsx
@@ -1,67 +1,18 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { useTheme } from '@shopify/restyle';
 import {
   LucideFlaskConical,
-  LucideUsers,
-  LucideMonitor,
   LucideFolder,
+  LucideMonitor,
+  LucideUsers,
 } from 'lucide-react-native';
-import { useTheme } from '@shopify/restyle';
-import type { Theme } from '../../../../styles/globalTheme/theme';
-import GroupSelect from '../../../../components/ui/GroupSelectModal';
 import type { ReactElement } from 'react';
+import React from 'react';
+import { Text, View } from 'react-native';
+import GroupSelect from '../../../../components/ui/GroupSelectModal';
+import type { Theme } from '../../../../styles/globalTheme/theme';
 import { GroupSelectTypes } from '../../../../types/uiTypes/GroupSelectTypes';
+import { createGroupCardStyles } from './styles/GroupCard.style';
 
-const createGroupCardStyles = (theme: Theme) => {
-  const { colors } = theme;
-
-  return StyleSheet.create({
-    card: {
-      display: 'flex',
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      width: '100%',
-      height: '100%',
-      paddingHorizontal: 15,
-      borderRadius: 12,
-      borderWidth: 1,
-      borderColor: '#2e2e2e',
-
-      zIndex: 1000,
-    },
-    label: {
-      flex: 1,
-      color: colors.textPrimary,
-      fontSize: 15,
-      fontWeight: '500',
-      height: '100%',
-      textAlign: 'center',
-    },
-    leftSection: {
-      display: 'flex',
-      width: '60%',
-      justifyContent: 'space-around',
-      alignItems: 'center',
-      flexDirection: 'row',
-    },
-    icon: {
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: 40,
-      padding: 10,
-      borderRadius: 10,
-    },
-    dropdownContainer: {
-      height: 40,
-      width: '35%',
-    },
-    GG: { backgroundColor: '#059669' }, // green
-    L: { backgroundColor: '#dc2626' }, // red
-    K: { backgroundColor: '#ea580c' }, // orange
-    P: { backgroundColor: '#4b5563' }, // gray
-  });
-};
 
 const iconMap: Record<string, ReactElement> = {
   GG: <LucideUsers size={20} color={'white'} />,
